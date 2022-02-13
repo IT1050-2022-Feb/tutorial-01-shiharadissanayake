aa9055f36ef4c6f616f393b168315422d4206497
shiharadissanayake <99583495+shiharadissanayake@users.noreply.github.com>
2022-02-13 13:35:17 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 
 int main() {
+   int m1,m2;
+   float average;
+   int sum;
+   printf("enter first mark");
+   scanf("%d",&m1);
+   printf("enter second mark")
+      scanf("%d",&m2);
+   sum=m1+m2;
+   average=sum/2.0;
+   printf("average is %2f",average);
+   
   
   return 0;
 }
